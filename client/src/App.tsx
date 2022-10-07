ca5bd1f2325460687c033601ee6f06b7c6454cf3
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-08 00:33:58 +0530

    add: added state mutater for username

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,38 @@
 import { useState } from "react";
+import { Input, ListGroup, ListGroupItem } from "reactstrap";
 
 function App() {
+  const [username, setUsername] = useState("");
+
   return (
     <div className="App">
-      <p>frontend</p>
+      <h1
+        style={{
+          margin: "1rem 4rem",
+          fontWeight: "bold",
+        }}
+      >
+        Pusher
+      </h1>
+      <div
+        style={{
+          maxWidth: "500px",
+          margin: "0 auto",
+        }}
+      >
+        <Input
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <ListGroup>
+        <ListGroupItem>Cras justo odio</ListGroupItem>
+        <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+        <ListGroupItem>Morbi leo risus</ListGroupItem>
+        <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
+        <ListGroupItem>Vestibulum at eros</ListGroupItem>
+      </ListGroup>
     </div>
   );
 }
