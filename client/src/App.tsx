abeab6258366ce20e44b8753157ef3bfad5e1f48
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-08 00:53:56 +0530

    y

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
-import { Input, ListGroup, ListGroupItem } from "reactstrap";
+import { Button, Input, ListGroup, ListGroupItem } from "reactstrap";
+import axios from "axios";
 
 function App() {
   const [username, setUsername] = useState("");
   const [messages, setMessages] = useState<string[]>([]);
   const [textMessage, setTextMessage] = useState("");
+
+  const submitMessage = async () => {
+    if (textMessage === "") return;
+    const response = await axios.post("/api/messages", {
+      message: textMessage,
+    });
+    console.log(response);
+    setTextMessage("");
+  };
 
   return (
     <div className="App">
@@ -45,7 +55,19 @@ function App() {
           value={textMessage}
           onChange={(e) => setTextMessage(e.target.value)}
           rows={4}
+          style={{
+            marginTop: "100%",
+          }}
         />
+        <Button
+          onClick={submitMessage}
+          color="primary"
+          style={{
+            marginTop: "1rem",
+          }}
+        >
+          Send
+        </Button>
       </div>
     </div>
   );
