d07c5bf106f7cf96a506facceb23e2e257e67a1b
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-07 22:57:03 +0530

    add: added custom logs for env

--- server/main.go
+++ server/main.go
@@ -3,11 +3,25 @@ package main
 //go:generate go run main.go
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	fmt.Printf("Starting server... %s", os.Getenv("DATA"))
 	app := fiber.New()
+
+	app.Use(cors.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
